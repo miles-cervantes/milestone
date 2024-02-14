9c03320a6ffdf25b2cd668951619ce445ec666f9
miles-cervantes <160008877+miles-cervantes@users.noreply.github.com>
2024-02-14 17:24:52 +0800

    Add files via upload

---
+++ script .js
@@ -0,0 +1,80 @@
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow(slidePosition = n);
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  var circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+      circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition-1].style.display = "block";
+  circles[slidePosition-1].className += " enable";
+} 
+
+function smoothScroll(target, duration) {
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = easing(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function easing(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 *t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+var home = document.querySelector('#home');
+var about = document.querySelector('#about');
+var services = document.querySelector('#services');
+var contact = document.querySelector('#contact');
+
+var homeLink = document.querySelector('nav a[href="#home"]');
+var aboutLink = document.querySelector('nav a[href="#about"]');
+var servicesLink = document.querySelector('nav a[href="#services"]');
+var contactLink = document.querySelector('nav a[href="#contact"]');
+
+homeLink.addEventListener('click', function() {
+    smoothScroll('#home', 1000);
+});
+
+aboutLink.addEventListener('click', function() {
+    smoothScroll('#about', 1000);
+});
+
+servicesLink.addEventListener('click', function() {
+    smoothScroll('#services', 1000);
+});
+
+contactLink.addEventListener('click', function() {
+    smoothScroll('#contact', 1000);
+});
+
